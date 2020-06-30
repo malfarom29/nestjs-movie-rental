8bbef7cbc8f61c5f17b7ac8592ae2262b2f34254
Manuel Alfaro <malfaro@applaudostudios.com>
2020-06-30 09:53:32 -0600

    ⚡️ Import new modules on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { MoviesModule } from './movies/movies.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { EmailModule } from './email/email.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), MoviesModule, EmailModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), MoviesModule, EmailModule, UserModule, AuthModule],
 })
 export class AppModule {}
