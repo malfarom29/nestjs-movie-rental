bd8a7a2fd94e4bca1f2e1a5d2e9520d81dc2e249
Manuel Alfaro <malfaro@applaudostudios.com>
2020-06-30 21:22:55 -0600

    ⚡️ Change aws values for env variables

--- src/config/aws-sdk.config.ts
+++ src/config/aws-sdk.config.ts
@@ -1,11 +1,8 @@
-import * as config from 'config';
 import * as AWS from 'aws-sdk';
 
-const awsConfig = config.get('aws');
-
 export const awsS3 = new AWS.S3({
-  accessKeyId: awsConfig.accessKeyId,
-  secretAccessKey: awsConfig.accessKeySecret,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
   useAccelerateEndpoint: true,
   signatureVersion: 'v4',
   region: 'us-east-2',
