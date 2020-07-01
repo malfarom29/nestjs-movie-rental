d20f16ca2bdbcd9e8bcd0ef5eb588187e6247d78
Manuel Alfaro <malfaro@applaudostudios.com>
2020-06-30 21:19:21 -0600

    ➕ Add new dependencies on auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 import { UserRepository } from 'src/user/user.repository';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
+import * as config from 'config';
+import { AuthRepository } from './auth.repository';
+
+const jwtConfig = config.get('jwt');
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    TypeOrmModule.forFeature([UserRepository, AuthRepository]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || jwtConfig.secret,
+      signOptions: {
+        expiresIn: jwtConfig.expiresIn,
+      },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
