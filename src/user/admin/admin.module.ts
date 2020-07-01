08c26ccb46d9dde14da373721b6023fd3cefa8ea
Manuel Alfaro <malfaro@applaudostudios.com>
2020-06-30 21:29:10 -0600

    ⚡️ Add admin module for admin users

---
+++ src/user/admin/admin.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MovieRepository } from 'src/movies/movie.repository';
+import { PassportModule } from '@nestjs/passport';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([MovieRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
+  controllers: [MoviesController],
+  providers: [MoviesService],
+})
+export class AdminModule {}
