dc7230fb83911e11551cef27f8964bd7fbc44c84
Manuel Alfaro <malfaro@applaudostudios.com>
2020-06-30 21:19:37 -0600

    ➕ Add new dependencies on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MoviesModule } from './movies/movies.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { EmailModule } from './email/email.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), MoviesModule, EmailModule, UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    MoviesModule,
+    EmailModule,
+    UserModule,
+    AuthModule,
+    OrderModule,
+  ],
 })
 export class AppModule {}
