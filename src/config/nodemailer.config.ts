dfc681d7bfe41e2db2779653bb0da0aef8fad0a0
Manuel Alfaro <malfaro@applaudostudios.com>
2020-06-30 21:23:09 -0600

    ⚡️ Change nodemailer values for env variables

--- src/config/nodemailer.config.ts
+++ src/config/nodemailer.config.ts
@@ -5,10 +5,10 @@ import * as config from 'config';
 const emailConfig = config.get('mailer');
 
 export const transporter: Mail = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST || emailConfig.host,
-  port: process.env.EMAIL_PORT || emailConfig.port,
+  host: process.env.MAILER_HOST || emailConfig.host,
+  port: process.env.MAILER_PORT || emailConfig.port,
   auth: {
-    user: process.env.EMAIL_USER || emailConfig.auth.user,
-    pass: process.env.EMAIL_PASSWORD || emailConfig.auth.password,
+    user: process.env.MAILER_USER || emailConfig.user,
+    pass: process.env.MAILER_PASSWORD || emailConfig.password,
   },
 });
