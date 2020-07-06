1527635872b1fcbcc5f962d5ac56aded1c43dd0f
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-05 19:46:51 -0600

    ⚡️ Add reset password functionality

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,16 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  Patch,
+  Param,
+} from '@nestjs/common';
 import { UserRegistrationDto } from 'src/user/dto/user-registration.dto';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { Auth } from '../database/entities/auth.entity';
+import { EmailValidatorPipe } from 'src/shared/pipes/email-validator.pipe';
 
 @Controller('auth')
 export class AuthController {
@@ -20,5 +28,20 @@ export class AuthController {
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
   ): Promise<Auth> {
     return this.authService.signIn(authCredentialsDto);
+  }
+
+  @Patch('/resetpassword')
+  requestResetPassword(
+    @Body('email', EmailValidatorPipe) email: string,
+  ): Promise<void> {
+    return this.authService.requestResetPassword(email);
+  }
+
+  @Patch('/resetpassword/:token')
+  resetPassword(
+    @Param('token') token: string,
+    @Body('newPassword') newPassword: string,
+  ): Promise<void> {
+    return this.authService.resetPassword(token, newPassword);
   }
 }
