4febc0a06a5c29cb703983a0410e2753d06e1fb4
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-05 19:47:35 -0600

    📝 Add api documentation on credentials dto

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,11 +1,14 @@
 import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthCredentialsDto {
+  @ApiProperty()
   @IsString()
   @MinLength(4)
   @MaxLength(20)
   username: string;
 
+  @ApiProperty()
   @IsString()
   @MinLength(8)
   @MaxLength(20)
