5e9a268c2d536d719a12d671b8e390452933dca5
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-05 19:42:17 -0600

    ⚡️ Add methods for user management on user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { AuthorizedUser } from 'src/shared/interfaces/authorized-user.interface';
+import { User } from './../database/entities/user.entity';
+import { PaginatedDataDto } from 'src/dtos/response/paginated-data.dto';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from 'src/repositories/user.repository';
+import { PaginationDto } from 'src/dtos/request/pagination.dto';
+import { UserResponseDto } from 'src/dtos/response/user-response.dto';
+import { plainToClass, plainToClassFromExist } from 'class-transformer';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {}
+
+  async getAllUsers(
+    paginationDto: PaginationDto,
+  ): Promise<PaginatedDataDto<UserResponseDto>> {
+    const { data, totalCount } = await this.userRepository.getAllUsers(
+      paginationDto,
+    );
+    const page = paginationDto.page || 1;
+    const limit = paginationDto.limit || 10;
+
+    const users = plainToClass(UserResponseDto, data);
+
+    return plainToClassFromExist(new PaginatedDataDto<UserResponseDto>(), {
+      data: users,
+      totalCount,
+      page,
+      limit,
+    });
+  }
+
+  async getUserById(id: number): Promise<UserResponseDto> {
+    const user = await this.findUser(id);
+
+    return plainToClass(UserResponseDto, user);
+  }
+
+  private async findUser(id: number): Promise<User> {
+    const user = await this.userRepository.findOne({ id });
+
+    if (!user) {
+      throw new NotFoundException(`User with ID "${id}" not found`);
+    }
+
+    return user;
+  }
+
+  async changeUserRoles(
+    id: number,
+    roles: string[],
+    user: AuthorizedUser,
+  ): Promise<UserResponseDto> {
+    if (id === user.userId) {
+      throw new BadRequestException(`You cannot change your own roles`);
+    }
+
+    const changedUser = await this.userRepository.changeUserRoles(id, roles);
+
+    return plainToClass(UserResponseDto, changedUser);
+  }
+}
