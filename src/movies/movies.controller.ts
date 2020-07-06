e38521a94f2ada94c7d45435f3052419fc286225
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-05 20:42:33 -0600

    ✨ Add method to like a movie

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,27 +1,61 @@
-import { Controller, Get, Query, Param, ParseIntPipe } from '@nestjs/common';
+import { RolesGuard } from './../shared/guards/roles.guard';
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+  Post,
+} from '@nestjs/common';
 import { MoviesService } from './movies.service';
-import { Movie } from '../database/entities/movie.entity';
-import { MoviesPaginationDto } from './dto/movies-pagination.dto';
-import * as config from 'config';
-import { PaginatedMoviesDto } from './dto/paginated-movies.dto';
-import { MovieResponseDto } from './dto/response/movie-response.dto';
+import { PaginationDto } from '../dtos/request/pagination.dto';
+import { PaginatedDataDto } from '../dtos/response/paginated-data.dto';
+import { MovieResponseDto } from 'src/dtos/response/movie-response.dto';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { GetUser } from 'src/shared/decorators/get-user.decorator';
+import { AuthorizedUser } from 'src/shared/interfaces/authorized-user.interface';
+import { Roles } from 'src/shared/decorators/roles.decorator';
+import { OptionalAuthGuard } from 'src/shared/guards/optional-auth.guard';
+import { AuthGuard } from '@nestjs/passport';
+import { FilterDto } from 'src/dtos/request/filter.dto';
+import { MovieFilterDto } from 'src/dtos/request/filters/movie-filter.dto';
 
 @Controller('movies')
+@UseGuards(OptionalAuthGuard, RolesGuard)
+@Roles()
 export class MoviesController {
-  private readonly awsConfig = config.get('aws');
   constructor(private moviesService: MoviesService) {}
 
   @Get()
   getMovies(
-    @Query() moviesPaginationDto: MoviesPaginationDto,
-  ): Promise<PaginatedMoviesDto> {
-    return this.moviesService.getMovies(moviesPaginationDto);
+    @Query() paginationDto: PaginationDto,
+    @Query() filterDto: FilterDto<MovieFilterDto>,
+    @GetUser() user?: AuthorizedUser,
+  ): Promise<PaginatedDataDto<MovieResponseDto>> {
+    return this.moviesService.getMovies(paginationDto, filterDto, user);
   }
 
+  @ApiTags('movies')
+  @ApiResponse({
+    description: 'Get movie by id. Anyone can access to this endpoint',
+    status: 200,
+    type: MovieResponseDto,
+  })
   @Get('/:id')
   getMovieById(
     @Param('id', ParseIntPipe) id: number,
+    @GetUser() user?: AuthorizedUser,
   ): Promise<MovieResponseDto> {
-    return this.moviesService.getMovieById(id);
+    return this.moviesService.getMovieById(id, user);
+  }
+
+  @Post('/:id/like')
+  @UseGuards(AuthGuard())
+  likeAMovie(
+    @Param('id', ParseIntPipe) id: number,
+    @GetUser() user: AuthorizedUser,
+  ): Promise<void> {
+    return this.moviesService.likeAMovie(id, user);
   }
 }
