127ba4075f92897e4df3458dce64d4ff45727638
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-06 09:05:46 -0600

    🎨 Change rental order dto location

--- src/dtos/response/rental-order.dto.ts
+++ src/dtos/response/rental-order.dto.ts
@@ -1,13 +1,9 @@
-import { Movie } from 'src/database/entities/movie.entity';
 import { Expose, Exclude } from 'class-transformer';
 
 @Exclude()
 export class RentalOrderDto {
   @Expose()
   id: number;
-
-  @Expose()
-  movie: Movie;
 
   @Expose()
   rentedAt: Date;
