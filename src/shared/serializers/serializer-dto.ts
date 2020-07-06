cf8129216de7eda92cb441424112e84120f204bc
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-06 15:01:35 -0600

    ✨ Add SerializerDto interface

---
+++ src/shared/serializers/serializer-dto.ts
@@ -0,0 +1,7 @@
+import { OrderResponseDto } from 'src/shared/dtos/response/order-response.dto';
+import { PaginatedDataDto } from 'src/shared/dtos/response/paginated-data.dto';
+
+export interface SerializerDto<V, D = any> {
+  serialize(...args: any): OrderResponseDto<V> | V;
+  serialize<V>(...args: any): OrderResponseDto<V> | PaginatedDataDto<V> | V;
+}
