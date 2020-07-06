5b35558d63cf2554fe9a1c06a5c6e51bce5cdba1
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-05 20:45:14 -0600

    🗃 Create movie factory for dummy data

---
+++ src/database/factories/movie.factory.ts
@@ -0,0 +1,14 @@
+import { Movie } from './../entities/movie.entity';
+import Faker from 'faker';
+import { define } from 'typeorm-seeding';
+
+define(Movie, (faker: typeof Faker) => {
+  const movie = new Movie();
+  movie.title = faker.random.words(4);
+  movie.description = faker.lorem.paragraph(2);
+  movie.stock = faker.random.number(25);
+  movie.salePrice = faker.random.number(40.0);
+  movie.rentalPrice = faker.random.number(4);
+
+  return movie;
+});
