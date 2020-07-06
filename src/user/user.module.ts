04e1c9886c5e08a3ba85a74532f390480ed6769e
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-05 19:42:38 -0600

    ⚡️ Import passport module on user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,18 @@
+import { PassportModule } from '@nestjs/passport';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { UserRepository } from '../repositories/user.repository';
 import { AdminModule } from './admin/admin.module';
+import { UserController } from './user.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository]), AdminModule],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    TypeOrmModule.forFeature([UserRepository]),
+    AdminModule,
+  ],
   providers: [UserService],
+  controllers: [UserController],
 })
 export class UserModule {}
