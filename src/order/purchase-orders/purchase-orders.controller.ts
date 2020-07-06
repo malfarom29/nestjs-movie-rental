449115ad5df2748d2915ea6bfd6a441a12139ff6
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-05 19:53:29 -0600

    ✨ Add purchase controller

---
+++ src/order/purchase-orders/purchase-orders.controller.ts
@@ -0,0 +1,46 @@
+import { User } from './../../database/entities/user.entity';
+import { PurchaseOrder } from './../../database/entities';
+import { PurchaseOrdersService } from './purchase-orders.service';
+import { UserRoles } from './../../shared/constants';
+import { RolesGuard } from './../../shared/guards/roles.guard';
+import { AuthGuard } from '@nestjs/passport';
+import {
+  Controller,
+  UseGuards,
+  Post,
+  ParseIntPipe,
+  Param,
+  Body,
+  Get,
+  Query,
+  Request,
+} from '@nestjs/common';
+import { Roles } from 'src/shared/decorators/roles.decorator';
+import { GetUser } from 'src/shared/decorators/get-user.decorator';
+import { PaginationDto } from 'src/dtos/request/pagination.dto';
+import { PaginatedDataDto } from 'src/dtos/response/paginated-data.dto';
+import { AuthorizedUser } from 'src/shared/interfaces/authorized-user.interface';
+
+@Controller('purchase-orders')
+@UseGuards(AuthGuard(), RolesGuard)
+@Roles(UserRoles.CUSTOMER)
+export class PurchaseOrdersController {
+  constructor(private purchaseOrdersService: PurchaseOrdersService) {}
+
+  @Post('/movie/:id')
+  purchaseOrder(
+    @Body('quantity', ParseIntPipe) quantity: number,
+    @Param('id', ParseIntPipe) id: number,
+    @GetUser() user: AuthorizedUser,
+  ): Promise<PurchaseOrder> {
+    return this.purchaseOrdersService.purchaseMovie(id, quantity, user);
+  }
+
+  @Get()
+  getMyPurchaseOrders(
+    @Query() paginationDto: PaginationDto,
+    @GetUser() user: AuthorizedUser,
+  ): Promise<PaginatedDataDto<PurchaseOrder>> {
+    return this.purchaseOrdersService.getMyPurchaseOrders(user, paginationDto);
+  }
+}
