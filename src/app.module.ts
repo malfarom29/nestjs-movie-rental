ae561fe352bac06dee99df9799fdbfffa22c2453
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-05 19:48:13 -0600

    ⚡️ Fix TypeOrmModule.forRoot functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { EmailModule } from './email/email.module';
+import { EmailModule } from './config/email/email.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
-import { loadTypeOrmConnectionFromEnv } from './loader';
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: () => {
-        return loadTypeOrmConnectionFromEnv();
-      },
-    }),
+    TypeOrmModule.forRoot(),
     MoviesModule,
     EmailModule,
     UserModule,
