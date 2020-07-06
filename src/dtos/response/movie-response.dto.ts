0222e66c8118009604234e88300977ee3210b397
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-05 19:50:44 -0600

    ⚡️ Add movie response dto for serialization

---
+++ src/dtos/response/movie-response.dto.ts
@@ -0,0 +1,59 @@
+import { MovieAttachment } from '../../database/entities/movie-attachment.entity';
+import { UserRoles } from '../../shared/constants';
+import { Exclude, Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+import * as aws from '../../config/aws/utils';
+
+@Exclude()
+export class MovieResponseDto {
+  @ApiProperty()
+  @Expose()
+  id: number;
+
+  @ApiProperty()
+  @Expose()
+  title: string;
+
+  @ApiProperty()
+  @Expose()
+  description: string;
+
+  @ApiProperty()
+  @Expose()
+  stock: number;
+
+  @ApiProperty({
+    required: false,
+  })
+  @Expose({ groups: [UserRoles.ADMIN] })
+  onRent: number;
+
+  @ApiProperty()
+  @Expose()
+  salePrice: number;
+
+  @ApiProperty()
+  @Expose()
+  rentalPrice: number;
+
+  @ApiProperty({
+    required: false,
+  })
+  @Expose({ groups: [UserRoles.ADMIN] })
+  availability: boolean;
+
+  @ApiProperty({
+    required: false,
+  })
+  @Expose({ groups: [UserRoles.ADMIN] })
+  dailyPenalty: number;
+
+  @ApiProperty({
+    required: false,
+  })
+  @Expose()
+  imageUrl?: string;
+
+  @Expose()
+  totalVotes: number;
+}
