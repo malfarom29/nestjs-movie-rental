90ae8c964a7264f851e5bfa96de6376cbf352380
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-06 23:56:09 -0600

    ⚡️ Check for user purchases

--- src/repositories/purchase-order.repository.ts
+++ src/repositories/purchase-order.repository.ts
@@ -1,6 +1,10 @@
-import { PurchaseOrder } from '../database/entities';
+import { PurchaseOrder, User } from '../database/entities';
 import { Movie } from '../database/entities';
-import { Logger, InternalServerErrorException } from '@nestjs/common';
+import {
+  Logger,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { PaginationDto } from 'src/shared/dtos/request/pagination.dto';
 
@@ -29,10 +33,15 @@ export class PurchaseOrderRepository extends Repository<PurchaseOrder> {
     return purchase;
   }
 
-  async getMyPurchaseOrders(
+  async getUserPurchaseOrders(
     userId: number,
     paginationDto: PaginationDto,
   ): Promise<{ data: PurchaseOrder[]; totalCount: number }> {
+    const user = await User.findOne({ id: userId });
+    if (!user) {
+      throw new NotFoundException(`User with ID "${userId}" not found`);
+    }
+
     const page = paginationDto.page || 1;
     const limit = paginationDto.limit || 10;
     const skip = (page - 1) * limit;
