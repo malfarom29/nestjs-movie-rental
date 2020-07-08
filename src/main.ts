5a0fcd3d617f1e71ceef212e7ce73e8bee61df86
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-08 12:49:29 -0600

    ⚡️ Add validation pipe on main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({}));
+
   app.setGlobalPrefix('/api/v1');
 
   const options = new DocumentBuilder()
