e59b9b7c3e372ae4c7c0f19bdd91d0047b55e97e
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-08 14:25:52 -0600

    🎨 Add rentals controller for admin

---
+++ src/admin/controllers/orders/rentals.controllers.ts
@@ -0,0 +1,33 @@
+import { RentalOrdersService } from './../../../customer/rental-orders.service';
+import { RentalOrderDto } from './../../../shared/dtos/response/rental-order.dto';
+import { OrderResponseDto } from './../../../shared/dtos/response/order-response.dto';
+import { PaginatedDataDto } from './../../../shared/dtos/response/paginated-data.dto';
+import { RolesGuard } from './../../../shared/guards/roles.guard';
+import { WhitelistTokenGuard } from './../../../shared/guards/whitelist-token.guard';
+import {
+  Controller,
+  UseGuards,
+  Get,
+  Param,
+  Query,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Roles } from 'src/shared/decorators/roles.decorator';
+import { UserRoles } from 'src/shared/constants';
+import { PaginationDto } from 'src/shared/dtos/request/pagination.dto';
+
+@Controller('admin/orders/rentals')
+@UseGuards(AuthGuard(), WhitelistTokenGuard, RolesGuard)
+@Roles(UserRoles.ADMIN)
+export class RentalsController {
+  constructor(private rentalOrdersService: RentalOrdersService) {}
+
+  @Get('/users/:id')
+  getUserRentalOrders(
+    @Param('id', ParseIntPipe) id: number,
+    @Query() paginationDto: PaginationDto,
+  ): Promise<PaginatedDataDto<OrderResponseDto<RentalOrderDto>[]>> {
+    return this.rentalOrdersService.getUserRentalOrders(id, paginationDto);
+  }
+}
