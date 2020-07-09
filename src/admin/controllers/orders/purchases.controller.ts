87c1b0ddb14f2bdde188d9a54bcc96f1ffad4225
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-09 12:01:41 -0600

    ✨ Add purchase controller for admin

---
+++ src/admin/controllers/orders/purchases.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Controller,
+  UseGuards,
+  Get,
+  Query,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { WhitelistTokenGuard } from 'src/shared/guards/whitelist-token.guard';
+import { RolesGuard } from 'src/shared/guards/roles.guard';
+import { Roles } from 'src/shared/decorators/roles.decorator';
+import { UserRoles } from 'src/shared/constants';
+import { PurchaseOrdersService } from 'src/services/purchase-orders.service';
+import { PaginationDto } from 'src/shared/dtos/request/pagination.dto';
+import { PaginatedDataDto } from 'src/shared/dtos/response/paginated-data.dto';
+import { OrderResponseDto } from 'src/shared/dtos/response/order-response.dto';
+import { PurchaseResponseDto } from 'src/shared/dtos/response/purchase-response.dto';
+
+@Controller('admin/orders/purchases')
+@UseGuards(AuthGuard(), WhitelistTokenGuard, RolesGuard)
+@Roles(UserRoles.ADMIN)
+export class PurchasesController {
+  constructor(private purchaseOrdersService: PurchaseOrdersService) {}
+
+  @Get('/users/:id')
+  getUserPurchaseOrders(
+    @Query() paginationDto: PaginationDto,
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<PaginatedDataDto<OrderResponseDto<PurchaseResponseDto>[]>> {
+    return this.purchaseOrdersService.getUserPurchaseOrders(id, paginationDto);
+  }
+}
