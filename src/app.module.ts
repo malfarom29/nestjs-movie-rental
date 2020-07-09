691a2186d9671794c3b34ab7887d20d4a8193dda
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-08 21:13:38 -0600

    🔥 Remove movies module from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { MoviesModule } from './movies/movies.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmailModule } from './config/email/email.module';
 import { UserModule } from './user/user.module';
@@ -12,7 +11,6 @@ import { CommonModule } from './common/common.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
-    MoviesModule,
     EmailModule,
     UserModule,
     AuthModule,
