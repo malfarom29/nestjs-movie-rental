3e9d1866dcfe98b5747aedfa3acd5b1bec32a75b
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-09 10:31:26 -0600

    🎨 Add general movie mapper instead of a admin mapper

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -12,11 +12,13 @@ import { OrderSerializer } from 'src/shared/serializers/order-serializer';
 import { PaginatedSerializer } from 'src/shared/serializers/paginated-serializer';
 import { MovieRepository } from 'src/repositories/movie.repository';
 import { MovieAttachmentRepository } from 'src/repositories/movie-attachment.repository';
-import { AdminMovieImageMapper } from 'src/shared/mappers/admin-movie-image.mapper';
 import { MovieSerializer } from 'src/shared/serializers/movie-serializer';
 import { UsersController } from './controllers/users/users.controller';
 import { UserSerializer } from 'src/shared/serializers/user-serializer';
 import { MoviesController } from './controllers/movies/movies.controller';
+import { MoviesService } from 'src/services/movies.service';
+import { VoteRepository } from 'src/repositories/votes.repository';
+import { MovieImageMapper } from 'src/shared/mappers/movie-image.mapper';
 
 @Module({
   imports: [
@@ -27,18 +29,20 @@ import { MoviesController } from './controllers/movies/movies.controller';
       MovieAttachmentRepository,
       RentalOrderRepository,
       ReturnOrderRepository,
+      VoteRepository,
     ]),
   ],
   controllers: [RentalsController, UsersController, MoviesController],
   providers: [
     Logger,
     UserService,
+    MoviesService,
     RentalOrdersService,
     UserSerializer,
     OrderSerializer,
     MovieSerializer,
     PaginatedSerializer,
-    AdminMovieImageMapper,
+    MovieImageMapper,
   ],
 })
 export class AdminModule {}
