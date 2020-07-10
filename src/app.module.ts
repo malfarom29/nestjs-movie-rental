ad62357ba59d82a7d0fc86bd6fa3f5d68d30d200
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-09 19:32:33 -0600

    ⚡️ Add Users Module to App Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmailModule } from './config/email/email.module';
-
 import { AuthModule } from './auth/auth.module';
 import { AdminModule } from './admin/admin.module';
 import { CustomerModule } from './customer/customer.module';
-import { CommonModule } from './common/common.module';
 import { MoviesModule } from './movies/movies.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -15,8 +14,8 @@ import { MoviesModule } from './movies/movies.module';
     AuthModule,
     AdminModule,
     CustomerModule,
-    CommonModule,
     MoviesModule,
+    UsersModule,
   ],
 })
 export class AppModule {}
