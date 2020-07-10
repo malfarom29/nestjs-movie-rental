3e4e8dff1de12d367ef6490bfbb909f86e03ac55
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-09 19:31:09 -0600

    ⚡️ Improve roles guard to let non-logged users access to shared endpoints

--- src/shared/guards/roles.guard.ts
+++ src/shared/guards/roles.guard.ts
@@ -8,11 +8,16 @@ export class RolesGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const target = [context.getClass(), context.getHandler()];
     const roles = this.reflector.getAllAndOverride<string[]>('roles', target);
+
     if (!roles.length) {
       return true;
     }
 
     const { user } = context.switchToHttp().getRequest();
+
+    if (!user) {
+      return true;
+    }
 
     const hasRole = user.roles.some(userRole =>
       roles.find(role => role === userRole),
