c76b4799003d701558a1fb57cad84682dccdc82b
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-10 11:09:36 -0600

    🗃 Change column name and make it not null

---
+++ src/database/migrations/1594396661808-ChangeRevokedAtColumnName.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class ChangeRevokedAtColumnName1594396661808 implements MigrationInterface {
+    name = 'ChangeRevokedAtColumnName1594396661808'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "auth" RENAME COLUMN "revokedAt" TO "refreshExpiresAt"`);
+        await queryRunner.query(`ALTER TABLE "auth" ALTER COLUMN "refreshExpiresAt" SET NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "auth" ALTER COLUMN "refreshExpiresAt" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "auth" RENAME COLUMN "refreshExpiresAt" TO "revokedAt"`);
+    }
+
+}
