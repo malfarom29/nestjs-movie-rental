9d3f7ea262e903d55aef98b8ebc5f48ce739106d
Manuel Alfaro <malfaro@applaudostudios.com>
2020-07-17 11:32:39 -0600

    🚀 Move bash file to before install

--- install-dcompose.sh
+++ install-dcompose.sh
@@ -6,3 +6,6 @@ sudo rm /usr/local/bin/docker-compose
 curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
 chmod +x docker-compose
 sudo mv docker-compose /usr/local/bin
+
+curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version 1.22.4
+export PATH="$HOME/.yarn/bin:$PATH"
